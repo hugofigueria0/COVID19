7e6d1a432e09e19f7753572848b235103d574d78
hugofigueira0 <hugofigueira0@gmail.com>
2020-06-05 20:18:11 +0100

    Calendário

--- app/src/main/java/pt/ipg/covid_19/MenuAdicionarMovimento.java
+++ app/src/main/java/pt/ipg/covid_19/MenuAdicionarMovimento.java
@@ -1,14 +1,26 @@
 package pt.ipg.covid_19;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MenuAdicionarMovimento extends AppCompatActivity {
     private EditText editDataEntrada;
 
+    private static final String TAG = "MenuAdicionarMovimento";
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,36 @@ public class MenuAdicionarMovimento extends AppCompatActivity {
 
         editDataEntrada = (EditText) findViewById(R.id.DataEntrada);
 
+        mDisplayDate = (TextView) findViewById(R.id.SelectDate);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MenuAdicionarMovimento.this,
+                        android.R.style.Widget_DeviceDefault_Light_ActionBar_Solid,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
 
 
     }
