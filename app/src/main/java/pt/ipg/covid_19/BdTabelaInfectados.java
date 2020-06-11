01c0da24c88367ecb94d845ba6fce12c072f1317
hugofigueira0 <hugofigueira0@gmail.com>
2020-06-11 15:51:35 +0100

    Criação da tabela movimentos

--- app/src/main/java/pt/ipg/covid_19/BdTabelaInfectados.java
+++ app/src/main/java/pt/ipg/covid_19/BdTabelaInfectados.java
@@ -1,7 +1,90 @@
 package pt.ipg.covid_19;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
+import android.text.TextUtils;
+
+import java.util.Arrays;
 
 public class BdTabelaInfectados implements BaseColumns {
-    
+
+    public static final String NOME_TABELA ="infectados";
+    public static final String CAMPO_INFECTADO = "infectado";
+
+    //Chave Estrangeira
+    public static final String CAMPO_ID_PESSOA = "id_pessoa";
+    public static final String CAMPO_PESSOA = "pessoa";
+
+    public static final String CAMPO_ID_COMPLETO =  NOME_TABELA + "." + _ID;
+    public static final String CAMPO_INFECTADOCOMPLETO = NOME_TABELA + "." + CAMPO_INFECTADO;
+
+    //Chave Estrangeira
+    public static final String CAMPO_ID_PESSOA_COMPLETO = NOME_TABELA + "." + CAMPO_ID_PESSOA;
+    public static final String CAMPO_PESSOA_COMPLETO = BdTabelaPessoas.CAMPO_NOME_COMPLETO + " AS " + CAMPO_PESSOA;
+
+    public static final String[] TODOS = {CAMPO_ID_COMPLETO, CAMPO_INFECTADOCOMPLETO, CAMPO_ID_PESSOA_COMPLETO, CAMPO_PESSOA_COMPLETO};
+
+
+
+
+    private SQLiteDatabase db;
+
+    public BdTabelaInfectados (SQLiteDatabase db){this.db = db;}
+
+    public void cria(){
+        db.execSQL("CREATE TABLE " + NOME_TABELA + " ("+
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                CAMPO_INFECTADO + " TEXT NOT NULL," +
+                CAMPO_PESSOA + " TEXT NOT NULL," +
+                "FOREIGN KEY (" + CAMPO_PESSOA_COMPLETO + ") REFERENCES " +
+                BdTabelaPessoas.NOME_TABELA + "("+ BdTabelaPessoas._ID + ")" +
+                ")"
+        );
+    }
+
+    public long insert(ContentValues values){ return  db.insert(NOME_TABELA, null, values);}
+
+    public Cursor query(String[] columns, String selection,
+                        String[] selectionArgs, String groupBy, String having,
+                        String orderBy){
+
+        if (!Arrays.asList(columns).contains(CAMPO_PESSOA_COMPLETO)) {
+            return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy);
+        }
+
+        String campos = TextUtils.join(",", columns);
+
+        String sql = "SELECT " + campos;
+        sql += " FROM " + NOME_TABELA + " INNER JOIN " + BdTabelaPessoas.NOME_TABELA;
+        sql += " ON " + CAMPO_ID_PESSOA_COMPLETO + "=" + BdTabelaPessoas.CAMPO_ID_COMPLETO;
+
+        if (selection != null) {
+            sql += " WHERE " + selection;
+        }
+
+        if (groupBy != null) {
+            sql += " GROUP BY " + groupBy;
+
+            if (having != null) {
+                sql += " HAVING " + having;
+            }
+        }
+
+        if (orderBy != null) {
+            sql += " ORDER BY " + orderBy;
+        }
+
+        return db.rawQuery(sql, selectionArgs);
+
+    }
+
+    public int update( ContentValues values, String whereClause, String[] whereArgs){
+        return db.update(NOME_TABELA, values, whereClause, whereArgs);
+    }
+
+    public int delete (String whereClause, String[] whereArgs){
+        return db.delete(NOME_TABELA, whereClause, whereArgs);
+    }
 }
---
+++ app/src/main/java/pt/ipg/covid_19/InfectadoModel.java
@@ -0,0 +1,41 @@
+package pt.ipg.covid_19;
+
+public class InfectadoModel {
+
+    private long id = -1;
+    private String infectado;
+    private long id_pessoa = -1;
+    private String nome_pessoa = null;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getInfectado() {
+        return infectado;
+    }
+
+    public void setInfectado(String infectado) {
+        this.infectado = infectado;
+    }
+
+    public long getId_pessoa() {
+        return id_pessoa;
+    }
+
+    public void setId_pessoa(long id_pessoa) {
+        this.id_pessoa = id_pessoa;
+    }
+
+    public String getNome_pessoa() {
+        return nome_pessoa;
+    }
+
+    public void setNome_pessoa(String nome_pessoa) {
+        this.nome_pessoa = nome_pessoa;
+    }
+}
