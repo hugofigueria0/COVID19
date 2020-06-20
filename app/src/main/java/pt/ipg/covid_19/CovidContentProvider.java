b8abfd9c0451720c5066af4581939c8713415e71
hugofigueira0 <hugofigueira0@gmail.com>
2020-06-20 15:10:01 +0100

    Conclusão do ContentProvider

--- app/src/main/java/pt/ipg/covid_19/CovidContentProvider.java
+++ app/src/main/java/pt/ipg/covid_19/CovidContentProvider.java
@@ -2,8 +2,11 @@ package pt.ipg.covid_19;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.ContentObserver;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.os.Build;
@@ -27,8 +30,8 @@ public class CovidContentProvider extends ContentProvider {
     private static final int URI_MOVIMENTO = 200;
     private static final int URI_ID_MOVIMENTO = 201;
 
-    private static final int URI_INFECTADO = 200;
-    private static final int URI_ID_INFECTADO= 201;
+    private static final int URI_INFECTADO = 400;
+    private static final int URI_ID_INFECTADO= 401;
 
 
     private static final String CURSOR_DIR = "vnd.android.cursor.dir/";
@@ -36,42 +39,159 @@ public class CovidContentProvider extends ContentProvider {
 
     private BdPessoasOpenHelper openHelper;
 
+    private UriMatcher getUriMatcher(){
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(AUTHORITY, PESSOAS, URI_PESSOAS);
+        uriMatcher.addURI(AUTHORITY, PESSOAS + "/#", URI_ID_PESSOAS);
+
+        uriMatcher.addURI(AUTHORITY, MOVIMENTO, URI_MOVIMENTO);
+        uriMatcher.addURI(AUTHORITY, MOVIMENTO + "/#", URI_ID_MOVIMENTO);
+
+        uriMatcher.addURI(AUTHORITY, INFECTADO, URI_INFECTADO);
+        uriMatcher.addURI(AUTHORITY, INFECTADO + "/#", URI_ID_INFECTADO);
+
+        return uriMatcher;
+    }
 
     @Override
     public boolean onCreate() {
-        return false;
+        openHelper = new BdPessoasOpenHelper(getContext());
+
+        return true;
     }
 
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+        SQLiteDatabase bd = openHelper.getReadableDatabase();
+
+        String id = uri.getLastPathSegment();
+
+        switch (getUriMatcher().match(uri)) {
+            case URI_PESSOAS:
+                return new BdTabelaPessoas(bd).query(projection, selection, selectionArgs, null, null, sortOrder);
+
+            case URI_ID_PESSOAS:
+                return new BdTabelaPessoas(bd).query(projection, BdTabelaPessoas._ID + "=?", new String[] { id }, null, null, sortOrder);
+
+            case URI_MOVIMENTO:
+                return new BdTabelaMovimento(bd).query(projection, selection, selectionArgs, null, null, sortOrder);
+
+            case URI_ID_MOVIMENTO:
+                return new BdTabelaMovimento(bd).query(projection, BdTabelaMovimento._ID + "=?", new String[] { id }, null, null, sortOrder);
+
+            case URI_INFECTADO:
+                return new BdTabelaInfectados(bd).query(projection, selection, selectionArgs, null, null, sortOrder);
+
+            case URI_ID_INFECTADO:
+                return new BdTabelaInfectados(bd).query(projection, BdTabelaInfectados._ID + "=?", new String[] { id }, null, null, sortOrder);
+
+            default:
+                throw new UnsupportedOperationException("Endereço query inválido: " + uri.getPath());
+        }
+
+
     }
 
 
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+        int codigoUri = getUriMatcher().match(uri);
+
+        switch (codigoUri) {
+            case URI_PESSOAS:
+                return CURSOR_DIR + PESSOAS;
+            case URI_ID_PESSOAS:
+                return CURSOR_ITEM + PESSOAS;
+            case URI_MOVIMENTO:
+                return CURSOR_DIR + MOVIMENTO;
+            case URI_ID_MOVIMENTO:
+                return CURSOR_ITEM + MOVIMENTO;
+            case URI_INFECTADO:
+                return CURSOR_DIR + INFECTADO;
+            case URI_ID_INFECTADO:
+                return CURSOR_ITEM + INFECTADO;
+            default:
+                return null;
+        }
     }
 
 
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+        SQLiteDatabase bd = openHelper.getWritableDatabase();
+
+        long id;
+
+        switch (getUriMatcher().match(uri)) {
+            case URI_PESSOAS:
+                id = (new BdTabelaPessoas(bd).insert(values));
+                break;
+
+            case URI_MOVIMENTO:
+                id = (new BdTabelaMovimento(bd).insert(values));
+                break;
+
+            case URI_INFECTADO:
+                id = (new BdTabelaInfectados(bd).insert(values));
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Endereço insert inválido: " + uri.getPath());
+        }
+
+        if (id == -1) {
+            throw new SQLException("Não foi possível inserir o registo: " + uri.getPath());
+        }
+
+        return Uri.withAppendedPath(uri, String.valueOf(id));
     }
 
 
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+        SQLiteDatabase bd = openHelper.getWritableDatabase();
+
+        String id = uri.getLastPathSegment();
+
+        switch (getUriMatcher().match(uri)) {
+            case URI_ID_PESSOAS:
+                return new BdTabelaPessoas(bd).delete(BdTabelaPessoas._ID + "=?", new String[]{id});
+
+            case URI_ID_MOVIMENTO:
+                return new BdTabelaMovimento(bd).delete(BdTabelaMovimento._ID + "=?", new String[] { id });
+
+            case URI_ID_INFECTADO:
+                return new BdTabelaInfectados(bd).delete(BdTabelaMovimento._ID + "=?", new String[] { id });
+
+            default:
+                throw new UnsupportedOperationException("Endereço delete inválido: " + uri.getPath());
+        }
     }
 
 
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+        SQLiteDatabase bd = openHelper.getWritableDatabase();
+
+        String id = uri.getLastPathSegment();
+
+        switch (getUriMatcher().match(uri)) {
+            case URI_ID_PESSOAS:
+                return new BdTabelaPessoas(bd).update(values, BdTabelaPessoas._ID + "=?", new String[] { id });
+
+            case URI_ID_MOVIMENTO:
+                return new BdTabelaMovimento(bd).update(values,BdTabelaMovimento._ID + "=?", new String[] { id });
+
+                case URI_ID_INFECTADO:
+                return new BdTabelaInfectados(bd).update(values,BdTabelaInfectados._ID + "=?", new String[] { id });
+
+            default:
+                throw new UnsupportedOperationException("Endereço de update inválido: " + uri.getPath());
+        }
     }
 }
