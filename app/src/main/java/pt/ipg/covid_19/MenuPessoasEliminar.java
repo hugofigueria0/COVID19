d8df896684508da4fdd9aea81028db3474aaf276
hugofigueira0 <hugofigueira0@gmail.com>
2020-06-22 17:59:53 +0100

    Finalização do Eliminar

--- app/src/main/java/pt/ipg/covid_19/MenuPessoasEliminar.java
+++ app/src/main/java/pt/ipg/covid_19/MenuPessoasEliminar.java
@@ -5,21 +5,28 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class MenuPessoasEliminar extends AppCompatActivity {
-    private TextView textViewNome;
-    private Uri enderecoPessoaApagar;
+public class MenuPessoasEliminar extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+    private static final int ID_CURSOR_LOADER_PESSOAS = 0;
+    private TextView textViewNome, textViewTipoPessoa;
     private PessoasModel pessoasModel;
+    private Uri enderecoPessoaEliminar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +35,9 @@ public class MenuPessoasEliminar extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-      /*  TextView textViewNome = (TextView) findViewById(R.id.NomePessoa);
-        TextView textViewTipoPessoa = (TextView) findViewById(R.id.TipoPessoa);
+        TextView textViewNome = (TextView) findViewById(R.id.textViewNome);
+        TextView textViewTipoPessoa = (TextView) findViewById(R.id.textViewTipoPessoa);
+
 
         Intent intent = getIntent();
 
@@ -41,67 +49,58 @@ public class MenuPessoasEliminar extends AppCompatActivity {
             return;
         }
 
-        enderecoPessoaApagar = Uri.withAppendedPath(CovidContentProvider.ENDERECO_PESSOAS, String.valueOf(idPessoa));
+        enderecoPessoaEliminar = Uri.withAppendedPath(CovidContentProvider.ENDERECO_PESSOAS, String.valueOf(idPessoa));
 
-        Cursor cursor = getContentResolver().query(enderecoPessoaApagar, BdTabelaPessoas.TODOS, null, null, null);
+        Cursor cursor = getContentResolver().query(enderecoPessoaEliminar, BdTabelaPessoas.TODOS, null, null, null);
 
         if(!cursor.moveToNext()){
-            Toast.makeText(this,"Erro não foi possivel ler o Carro!!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"Erro não foi possivel ler o Pessoa!!", Toast.LENGTH_LONG).show();
             finish();
             return;
         }
 
-        PessoasModel pessoasModel = PessoasModel.fromCursor(cursor);
+
+        pessoasModel = PessoasModel.fromCursor(cursor);
 
         textViewNome.setText(pessoasModel.getNome());
         textViewTipoPessoa.setText(pessoasModel.getTipoPessoa());
 
 
-*/
     }
 
      public void Eliminar (View view) {
-         AlertDialog.Builder builder = new AlertDialog.Builder(this);
+         int PessoasApagadas = getContentResolver().delete(enderecoPessoaEliminar, null, null);
 
-         builder.setTitle("Eliminar Livro");
-         builder.setMessage("Tem a certeza que pretende eliminar o livro '" + pessoasModel.getNome() + "'");
-         builder.setIcon(R.drawable.ic_delete_black_24dp);
-         builder.setPositiveButton("Sim, eliminar", new DialogInterface.OnClickListener() {
-             @Override
-             public void onClick(DialogInterface dialog, int which) {
-                 confirmaEliminar();
-             }
-         });
+         if (PessoasApagadas == 1) {
+             Toast.makeText(this, "nao sei o que acontece", Toast.LENGTH_SHORT).show();
+             finish();
+         } else {
+             Toast.makeText(this, "aconteceu", Toast.LENGTH_LONG).show();
+         }
 
-         builder.setNegativeButton("Não, cancelar", new DialogInterface.OnClickListener() {
-             @Override
-             public void onClick(DialogInterface dialog, int which) {
-                 // cancelar
-             }
-         });
-
-         builder.show();
-    }
-
-    private void confirmaEliminar() {
-        try {
-            Uri enderecoPessoa = Uri.withAppendedPath(CovidContentProvider.ENDERECO_PESSOAS, String.valueOf(pessoasModel.getId()));
-
-            int apagados = this.getContentResolver().delete(enderecoPessoa, null, null);
-
-            if (apagados == 1) {
-                Toast.makeText(this, "Pessoa eliminada com sucesso", Toast.LENGTH_SHORT).show();
-                return;
-            }
-        } catch (Exception e) {
-        }
+     }
 
 
-        Snackbar.make(textViewNome, "Erro: Não foi possível eliminar o livro", Snackbar.LENGTH_INDEFINITE).show();
-    }
 
     public void Leave(View view){
         finish();
         Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show();
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+      //  return new CursorLoader(this, CovidContentProvider.ENDERECO_PESSOAS, BdTabelaPessoas.TODOS, null, null, BdTabelaPessoas.CAMPO_NOME);
+    return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
